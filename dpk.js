8d37d3de36687144479d965980724342b85d85a9
Kartik Arora <akartik80@gmail.com>
2023-03-04 17:02:09 +0800

    first done

--- dpk.js
+++ dpk.js
@@ -1,6 +1,59 @@
 const crypto = require("crypto");
 
+// Helper function to generate a hash key based on "hex" encoding and "sha3-512" algorithm
+exports.getPartitionKey = key => {
+  const HASH_ENCODING = "hex";
+  const HASH_ALGORITHM = "sha3-512";
+
+  if (!key) {
+    return null;
+  }
+
+  return crypto.createHash(HASH_ALGORITHM).update(key).digest(HASH_ENCODING);
+}
+
+// Helper function to sanitize a key with max key length as 256
+exports.getSanitizedKey = key => {
+  const MAX_PARTITION_KEY_LENGTH = 256;
+
+  if (!key) {
+    return null;
+  }
+
+  const sanitizedKey = (typeof key === "string") ? key : JSON.stringify(key);
+  return (sanitizedKey.length > MAX_PARTITION_KEY_LENGTH) ? this.getPartitionKey(sanitizedKey) : sanitizedKey;
+};
+
+// Main function to generate a deterministic partition key
 exports.deterministicPartitionKey = (event) => {
+  const DEFAULT_PARTITION_KEY = "0";
+
+  // If event is not defined or null, return the default partition key
+  if (!event) {
+    return DEFAULT_PARTITION_KEY;
+  }
+
+  // If the event already has a partition key, use that key
+  const candidateKey = event.partitionKey || this.getPartitionKey(JSON.stringify(event));
+  return this.getSanitizedKey(candidateKey) || DEFAULT_PARTITION_KEY;
+};
+
+
+/*
+Following are the reasons why my implementation is more readable than original implementation:
+1. The use of constant variables makes the code more flexible and easier to customize.
+2. The functions are simplified and made more concise using ES6 arrow functions
+3. The code is organized into smaller, more focused functions that have a single responsibility, making it easier to understand and maintain.
+4. The function names are more descriptive and provide better context for what they do.
+5. The variable naming is more consistent and easier to understand.
+6. The deterministicPartitionKey function is using the guard clause for event null check removing the cognitive overload of what happens when event is null in the code following that statement.
+*/
+
+
+/*
+Original implementation:
+
+exports.deterministicPartitionKey2 = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
@@ -26,3 +79,4 @@ exports.deterministicPartitionKey = (event) => {
   }
   return candidate;
 };
+*/
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,91 @@
-const { deterministicPartitionKey } = require("./dpk");
+const { deterministicPartitionKey, getPartitionKey, getSanitizedKey } = require("./dpk");
 
-describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
+describe('deterministicPartitionKey', () => {
+  test("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  test('should use the partition key from the event if it exists', () => {
+    const event = { partitionKey: 'abc' };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toEqual('abc');
+  });
+
+  test('should generate partition key for the event if partition key is not present', () => {
+    const event = { data: 'sample data' };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toEqual(expect.any(String));
+  });
+
+  test('should generate same partition key for the same event', () => {
+    const event = { data: 'sample data' };
+    const partitionKey1 = deterministicPartitionKey(event);
+    const partitionKey2 = deterministicPartitionKey(event);
+    expect(partitionKey1).toEqual(partitionKey2);
+  });
+
+  test('should generate different partition key for different events', () => {
+    const event1 = { data: 'sample data 1' };
+    const event2 = { data: 'sample data 2' };
+    const partitionKey1 = deterministicPartitionKey(event1);
+    const partitionKey2 = deterministicPartitionKey(event2);
+    expect(partitionKey1).not.toEqual(partitionKey2);
+  });
+
+  test('should generate partition key of string type', () => {
+    const event = { data: 'sample data' };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(typeof partitionKey).toEqual('string');
+  });
+
+  test('should handle empty object event', () => {
+    const event = {};
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toEqual(expect.any(String));
+  });
+
+  test('should handle empty string partition key', () => {
+    const event = { partitionKey: '' };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toEqual(expect.any(String));
+  });
+
+  test('should handle non-string partition key', () => {
+    const event = { partitionKey: { foo: 'bar' } };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toEqual(expect.any(String));
+  });
+
+  test('should handle very long partition key', () => {
+    const event = { data: 'a'.repeat(500) };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toEqual(expect.any(String));
+  });
 });
+
+describe("getPartitionKey", () => {
+  it("should return null if the input key is null", () => {
+    const key = null;
+    const expectedHash = null;
+    const actualHash = getPartitionKey(key);
+    expect(actualHash).toEqual(expectedHash);
+  });
+});
+
+describe('getSanitizedKey', () => {
+  test('returns null for empty input', () => {
+    expect(getSanitizedKey()).toBeNull();
+  });
+
+  test('returns the same string for string inputs within max length', () => {
+    const input = 'hello world';
+    expect(getSanitizedKey(input)).toBe(input);
+  });
+
+  test('returns the same JSON string for valid JSON inputs within max length', () => {
+    const input = { a: 1, b: 2 };
+    expect(getSanitizedKey(input)).toBe(JSON.stringify(input));
+  });
+});
+
